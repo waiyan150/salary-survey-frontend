d756d1915855ce1c50b1d848dc78331a5181f5d2
waiyan150 <46592143+waiyan150@users.noreply.github.com>
2023-11-12 19:32:46 +0630

    Add fix for looping of userEffect

--- src/components/SalarySurveyList.js
+++ src/components/SalarySurveyList.js
@@ -4,18 +4,26 @@ import TextField from '@mui/material/TextField';
 import { Container, Paper, Button } from '@mui/material';
 
 export default function SalarySurveryList() {
+    const initialized = React.useRef(false);
     const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' }
-    const [currentPage, setCurrentPage] = React.useState(0);
+    const [searchResults, setSearchResults] = React.useState(0);
     const [salarySurveyList, setSalarySurveyList] = React.useState([]);
+    const [results, setResults] = React.useState([]);
     React.useEffect(() => {
-        fetch("http://localhost:8080/salary-survey/survey")
-            .then(response => response.json())
-            .then((results) => {
-                console.log(results);
-                setSalarySurveyList(results._embedded.salarySurveyList)
-            })
-            .catch(error => console.error(error));
-    }, [currentPage])
+        if (!initialized.current) {
+            initialized.current = true
+            fetch("http://localhost:8080/salary-survey/survey")
+                .then(response => response.json())
+                .then((results) => {
+                    setResults(results)
+                    setSalarySurveyList(results._embedded.salarySurveyList)
+                    setSearchResults(results.page.totalElements)
+                })
+                .catch(error => console.error(error));
+        }
+    }, [])
+    console.log(results)
+    console.log(searchResults)
 
     return (
         // salarySurveyList.length > 0 ? null :
